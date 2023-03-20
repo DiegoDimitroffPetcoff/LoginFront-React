165ce4a486ff99c5d68695369ebad47f8350e221
Diego Dimitroff Petcoff <diegodimitroffpetcoff@gmail.com>
2023-03-20 13:06:12 +0000

    Add Login fornt with Reaact

---
+++ src/component/form.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+export default function Form() {
+  const [data, setData] = useState({
+    name: "",
+    password: "",
+  });
+
+const  HandleInputChange =(event) =>{
+
+setData({
+    //con ..data hago una pseudo copia del objeto que se encuentra en state
+    ...data,
+    [event.target.name]:event.target.value,
+   
+
+})
+}
+
+const HandleFormChange =(event) => {
+    event.preventDefault()
+    console.log(data)
+
+}
+
+
+
+  return (
+    <div>
+      {" "}
+      <form  onClick={HandleFormChange}>
+        <input placeholder="Name" type="text" name="name" onChange={HandleInputChange}></input>
+        <input placeholder="Password" type="password" name="password"  onChange={HandleInputChange}></input>
+        <button type="Submit" >Enviar</button>
+      </form>
+    </div>
+  );
+}
